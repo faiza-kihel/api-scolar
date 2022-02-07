dfb4f42a0e5d49065f7d6cf46d1bbe93ea6be1eb
faiza Kihel <faizakihel2@gmail.com>
2022-02-07 16:59:40 +0100

    authService

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
+import { UserModule } from './user/user/user.module';
+import { AuthModule } from './auth/auth.module';
 @Module({
   controllers: [AppController],
   providers: [AppService],
-  imports: [UserModule],
+  imports: [UserModule, AuthModule],
 })
 export class AppModule {}
--- src/user/user/user.module.ts
+++ src/user/user/user.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
-import { UserService } from './user/user.service';
-import { UserController } from './user/user.controller';
+import { UserService } from './user.service';
+import { UserController } from './user.controller';
 import { PrismaService } from 'prisma/prismaServices/prisma/prisma.service';
 
 @Module({
   providers: [UserService, PrismaService],
-  controllers: [UserController],
-  exports: [UserModule],
+  controllers: [UserController]
 })
 export class UserModule {}
